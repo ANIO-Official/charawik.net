bb5c0ef263e551e66e2647e56f59b4e10b1817f4
Amanda Ogletree <amandaogletree19@gmail.com>
2026-02-02 09:57:51 -0500

    Added Routes to App.tsx with temporary sample text for pages to test routing in browser. Added CSSs for all pages. Added route for Activity Modal.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css'
+import LandingPage from './pages/LandingPage/LandingPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+import ActivityPage from './pages/ActivityPage/ActivityPage';
+import CharacterPage from './pages/CharacterPage/CharacterPage';
+import CharacterCreationPage from './pages/CharacterCreationPage/CharacterCreationPage';
+import ProfilePage from './pages/ProfilePage/ProfilePage'
+import ActivityModal from './components/Character/ActivityModal/ActivityModal';
 
 function App() {
 
   return (
-    <>
-      App
+    <div id='application'>
     <Routes>
+      <Route path='/' element={<LandingPage/>} />
+      <Route path='/:username' element= {<ProfilePage/>} />
+      <Route path='/characters/create' element= {<CharacterCreationPage/>} />
+      <Route path='/characters/:characterId' element= {<CharacterPage/>} />
+      <Route path='/activities/create' Component={ActivityModal}/>
+      <Route path='/characters/:characterId/activities/:activityId' element= {<ActivityPage/>} />
+      <Route path='*' element= {<NotFoundPage/>} />
     </Routes>
-    </>
+    </div>
   )
 }
 
---
+++ src/pages/CharacterCreationPage/CharacterCreationPage.tsx
@@ -0,0 +1,7 @@
+export default function CharacterCreationPage(){
+    return(
+        <>
+            Character Creation Page
+        </>
+    )
+}
