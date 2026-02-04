9fd148da1213447a647407d116f2cba8c4b34c9d
Amanda Ogletree <amandaogletree19@gmail.com>
2026-02-04 14:12:33 -0500

    Added onCLick event to AddCharacter button to navigate user to Character Creation page on click.

--- src/components/Profile/AddCharacterButton/AddCharacterButton.tsx
+++ src/components/Profile/AddCharacterButton/AddCharacterButton.tsx
@@ -1,8 +1,12 @@
+import { useNavigate } from "react-router-dom";
 import "./AddCharacterButton.css";
 
 export default function AddCharacterButton() {
+  const navigate = useNavigate()
   return (
-    <button className="add-character-button al">
+    <button
+    onClick={() => navigate("/characters/create")}
+    className="add-character-button ">
       +
     </button>
   );
